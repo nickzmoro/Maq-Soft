3e655d8260f859981e13cae3f227169845be1ff0
nickzmoro <nicolasmororuiz@gmail.com>
2025-02-23 16:04:14 -0300

    chore(globalStyle): folder style for globalStyle.js

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        background-color: #fff;
+        margin: 0;
+        padding: 0;
+        overflow-x: hidden;
+    }
+
+    * {
+        font-family: 'Inter', sans-serif;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+        text-decoration: none;
+        border: none;
+    }
+
+    ::-webkit-scrollbar {
+        width: 8px; /* Largura do scrollbar */
+    }
+
+    ::-webkit-scrollbar-track {
+        background: transparent; /* Fundo do scrollbar transparente */
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: rgba(0, 0, 0, 0.1); /* Cor da "barra" do scrollbar (semi-transparente) */
+        border-radius: 4px;
+    }
+
+    /* Para Firefox */
+    * {
+        scrollbar-width: thin;
+        scrollbar-color: rgba(0, 0, 0, 0.5) transparent; /* Barra semi-transparente e fundo transparente */
+    }
+`;
