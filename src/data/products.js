b26d9f3842495ec17d05b9fad74c63e4c27f22aa
nickzmoro <nicolasmororuiz@gmail.com>
2025-03-05 23:06:05 -0300

    feat(products.js): add new products

--- src/data/products.js
+++ src/data/products.js
@@ -1,61 +1,526 @@
 import { v4 as uuidv4 } from "uuid";
 
 export const product = [
+  /* PICOLÉS */
   {
     id: uuidv4(),
-    name: "Super Sundae",
-    description:
-      "Sorvete expresso + geleia de morango + ganache de chocolate + farofa de amendoim + canudo waffer",
-    price: 14.99,
+    name: "Picolé de Iorgute de Morango",
+    description: "40g de picolé sabor iorgute de morango",
+    price: 1.5,
     imageUrl:
-      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDdAL9HlOP0wI9ALKhXMTjs2bnmVozY4DluFU1",
-  },
-  {
-    id: uuidv4(),
-    name: "Picolé",
-    description:
-      "Sorvete expresso + geleia de morango + ganache de chocolate + farofa de amendoim + canudo waffer",
-    price: 14.99,
-    imageUrl:
-      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDdAL9HlOP0wI9ALKhXMTjs2bnmVozY4DluFU1",
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDm554RZyCdwVurFPNiSlXADcB2aJzhvpneOGt",
     category: "picole",
   },
   {
     id: uuidv4(),
-    name: "Pote",
-    description:
-      "Sorvete expresso + geleia de morango + ganache de chocolate + farofa de amendoim + canudo waffer",
-    price: 14.99,
+    name: "Picolé de Leite Condensado",
+    description: "40g de picolé sabor leite condensado",
+    price: 1.5,
     imageUrl:
-      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDdAL9HlOP0wI9ALKhXMTjs2bnmVozY4DluFU1",
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDL0xJYlCesPVuz6kn9HYb1mxyAhlwf0NX83UR",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Mousse de Maracujá",
+    description: "40g de picolé sabor mousse de maracujá",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD4rE5ktj7lIwtmodEAZSqy9LpDjFUPNfkGBns",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Capuccino",
+    description: "40g de picolé sabor capuccino",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDIsJApCPBIWCEDFlTA5fuQsqwmdGcM7U34o86",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Nata",
+    description: "40g de picolé sabor nata",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDzYgTvu8CBGzraRp2nJHywvob74g38ZjcTPW9",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé Pinta Língua",
+    description: "40g de picolé que pinta a língua!",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDUFA7J5HYyzgRLXbs91jmGHWZvcDKSeihMF6a",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Chiclete",
+    description: "40g de picolé sabor chiclete",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD8dkAoyZQ2XoWUkVFjTKEu9IBybHfzerOGRDt",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Abacaxi",
+    description: "40g de picolé sabor abacaxi",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD4z9NQhj7lIwtmodEAZSqy9LpDjFUPNfkGBns",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Morango ao leite",
+    description: "40g de picolé sabor morango ao leite",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDsVkJKbTmTAUkK0Lmuv17q9VjFpBHPoxaQ5W8",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Mamão Papaya",
+    description: "40g de picolé sabor mamão papaya",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDp2FUr5iMvKu9Zg8srHqSdUDVPcWJCznbpRAi",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Ninho",
+    description: "40g de picolé sabor ninho",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD7KWH7apXd9qUwFGp5R42lx1jDrcOKbHzWtyv",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Limão Suíço",
+    description: "40g de picolé sabor limão suíço",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD6jAoL15QU5cQyTzdms7vr1NwPKoJElFOa9gA",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Amendoím",
+    description: "40g de picolé sabor amendoím",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDwxxr01bAPDSj4n0Ngv9O3EQhMCeJtokKraUT",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Acabaxi c/ Hortelã",
+    description: "40g de picolé sabor abacaxi com hortelã",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDkMolh9JytOML05KhsaFWD49JvE2rqfANRZUd",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Banana",
+    description: "40g de picolé sabor banana",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDrYjUnseJSw7KVGlUWcyO5beoBZkY0ILNQ3EP",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Uva",
+    description: "40g de picolé sabor uva",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDfX2D6MhTbn4dY7BefzV2hqxUkCIFgavl1OWP",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Morango",
+    description: "40g de picolé sabor morango",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDGjCCB3I24QB3uHwK8qDiEcrIaYUmx6g9yotM",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Uva ao leite",
+    description: "40g de picolé sabor uva ao leite",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDhZjuedrLzVcY7JwW8XdqmjTEOas9ABvC1nep",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Melão",
+    description: "40g de picolé sabor melão",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDIU1w7ePBIWCEDFlTA5fuQsqwmdGcM7U34o86",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Graviola",
+    description: "40g de picolé sabor graviola",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD7N5ovJBpXd9qUwFGp5R42lx1jDrcOKbHzWty",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Menta",
+    description: "40g de picolé sabor menta",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD7NEKOBypXd9qUwFGp5R42lx1jDrcOKbHzWty",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Abacaxi Suíço",
+    description: "40g de picolé sabor abacaxi suíço",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDTxEOdskOciaXLPCmg06ueQd7IMFTqhfYvoNK",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Jabuticaba",
+    description: "40g de picolé sabor jabuticaba",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDNZUTPuBRkyfr9MuQ24EbTqHhg3Kj78eiPapn",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Coco Queimado",
+    description: "40g de picolé sabor coco queimado",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD7GzdfqpXd9qUwFGp5R42lx1jDrcOKbHzWtyv",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Bala de Framboesa",
+    description: "40g de picolé sabor bala de framboesa",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDwlmprRbAPDSj4n0Ngv9O3EQhMCeJtokKraUT",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Coco",
+    description: "40g de picolé sabor coco",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD3PwjCLunp9NKZ6LmTFzVMOCXPdyIluhBicQ7",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Doce de Leite",
+    description: "40g de picolé sabor doce de leite",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDpBE2sE7iMvKu9Zg8srHqSdUDVPcWJCznbpRA",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Milho Verde",
+    description: "40g de picolé sabor milho verde",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDCkAgeBvbX7HqMpLJ4j3yn2zPGco1dkAFrew5",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Goiaba",
+    description: "40g de picolé sabor goiaba",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDxhRK0f4APDYpXdZqFcBE5lsrhbHvSxW7CnOQ",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Maracujá",
+    description: "40g de picolé sabor maracujá",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDYiqPRRS96EKNHbzqkfpSZ02vUg7s5dPtOwXT",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Melancia",
+    description: "40g de picolé sabor melancia",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDFjevwyDwO12naVpCgSKhQTE8y4zWZrdeiN3U",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Groselha",
+    description: "40g de picolé sabor groselha",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDG6ZRqZI24QB3uHwK8qDiEcrIaYUmx6g9yotM",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Limão",
+    description: "40g de picolé sabor limão",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD7soINdpXd9qUwFGp5R42lx1jDrcOKbHzWtyv",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Tangerina",
+    description: "40g de picolé sabor tangerina",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDNE4xDcyBRkyfr9MuQ24EbTqHhg3Kj78eiPap",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Chocolate Belga",
+    description: "40g de picolé sabor chocolate belga",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD3byphiunp9NKZ6LmTFzVMOCXPdyIluhBicQ7",
+    category: "picole",
+  },
+  {
+    id: uuidv4(),
+    name: "Picolé de Beijinho",
+    description: "40g de picolé sabor beijinho",
+    price: 1.5,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD1veKxwYB9HsJWzKMrZjdqkRt5yflLbVoQagP",
+    category: "picole",
+  },
+
+  /* MORENINHA */
+  {
+    id: uuidv4(),
+    name: "Moreninha Ninho",
+    description:
+      "Sorvete com casquinha e cobertura de chocolate com recheio de ninho",
+    price: 4,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDk8I263JytOML05KhsaFWD49JvE2rqfANRZUd",
+    category: "moreninha",
+  },
+  {
+    id: uuidv4(),
+    name: "Moreninha Flocos",
+    description:
+      "Sorvete com casquinha e cobertura de chocolate com recheio de flocos",
+    price: 4,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDHMyFEot3XdPrQBSItjFKOyEvM7ichpGbmV62",
+    category: "moreninha",
+  },
+  {
+    id: uuidv4(),
+    name: "Moreninha Chocolate",
+    description:
+      "Sorvete com casquinha e cobertura de chocolate com recheio de chocolate",
+    price: 4,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD8yfenpZQ2XoWUkVFjTKEu9IBybHfzerOGRDt",
+    category: "moreninha",
+  },
+  {
+    id: uuidv4(),
+    name: "Moreninha Morango",
+    description:
+      "Sorvete com casquinha e cobertura de chocolate com recheio de morango",
+    price: 4,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDgDAzt4GnHDxtaSXfOeciFpTJkvd4QUrzgCIy",
+    category: "moreninha",
+  },
+  {
+    id: uuidv4(),
+    name: "Moreninha Leite Condensado",
+    description:
+      "Sorvete com casquinha e cobertura de chocolate com recheio de leite condensado",
+    price: 4,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDFQHtMmEDwO12naVpCgSKhQTE8y4zWZrdeiN3",
+    category: "moreninha",
+  },
+
+  /* POTES */
+  {
+    id: uuidv4(),
+    name: "Pote de 1 Litro | Escolha seu sabor: ",
+    description:
+      "Temos diversos potes 1 litro de sorvete de massa: iogurte c/ amarena, abacaxi, etc..",
+    price: 20,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDdSkniJOP0wI9ALKhXMTjs2bnmVozY4DluFU1",
     category: "pote",
   },
+
+  /* CASQUINHAS */
   {
     id: uuidv4(),
-    name: "Açaí",
+    name: "Casquinha",
+    description:
+      "Sorvete expresso de sabor baunilha, chocolate ou mista + cobertura",
+    price: 4,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDZp5DtKlEQUgySX9aD18GcnzjbfAmxk0V5o2Y",
+    category: "casquinha",
+  },
+  {
+    id: uuidv4(),
+    name: "Cascão",
+    description:
+      "Sorvete expresso de sabor baunilha, chocolate ou mista + cobertura + canudo waffer",
+    price: 6,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDyfwv5rVz0FIVMfTijqKCHwNAO4GbnUdYQuLB",
+    category: "casquinha",
+  },
+  {
+    id: uuidv4(),
+    name: "Cascão Trufado",
+    description:
+      "Sorvete expresso de sabor baunilha, chocolate ou mista + chocolate em volta do cascão + cobertura + canudo waffer",
+    price: 7,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDHmyWij3XdPrQBSItjFKOyEvM7ichpGbmV62D",
+    category: "casquinha",
+  },
+
+  /* ESPECIAIS */
+  {
+    id: uuidv4(),
+    name: "Sundae",
     description:
       "Sorvete expresso + geleia de morango + ganache de chocolate + farofa de amendoim + canudo waffer",
-    price: 14.99,
+    price: 7,
     imageUrl:
-      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDdAL9HlOP0wI9ALKhXMTjs2bnmVozY4DluFU1",
-    category: "acai",
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDLk2UeLCesPVuz6kn9HYb1mxyAhlwf0NX83UR",
+    category: "especiais",
   },
   {
     id: uuidv4(),
     name: "Super Sundae",
     description:
       "Sorvete expresso + geleia de morango + ganache de chocolate + farofa de amendoim + canudo waffer",
-    price: 14.99,
+    price: 10,
     imageUrl:
-      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDdAL9HlOP0wI9ALKhXMTjs2bnmVozY4DluFU1",
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD6ws0a5QU5cQyTzdms7vr1NwPKoJElFOa9gAH",
+    category: "especiais",
   },
   {
     id: uuidv4(),
-    name: "Super Sundae",
+    name: "Sensação",
     description:
       "Sorvete expresso + geleia de morango + ganache de chocolate + farofa de amendoim + canudo waffer",
-    price: 14.99,
+    price: 10,
     imageUrl:
-      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDdAL9HlOP0wI9ALKhXMTjs2bnmVozY4DluFU1",
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDmePLjnyCdwVurFPNiSlXADcB2aJzhvpneOGt",
+    category: "especiais",
+  },
+  {
+    id: uuidv4(),
+    name: "Ouro Branco",
+    description:
+      "Sorvete expresso + cobertura + Ouro Branco + ganache + farofa de amendoím",
+    price: 10,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDMSWck6r1SsypANUmTcOvB0KV8PzIkb9t6oaD",
+    category: "especiais",
+  },
+  {
+    id: uuidv4(),
+    name: "Chanty Max",
+    description:
+      "Sorvete expresso + ganache chocolate + geleia morango + chantily",
+    price: 11,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDAqw6vdfhzeX4bdDxK06BYkQTySmG3jZ1gMrI",
+    category: "especiais",
+  },
+  {
+    id: uuidv4(),
+    name: "Chanty Neve",
+    description:
+      "Sorvete expresso + cobertura + geleia morango + suspiros + chantily",
+    price: 11,
+    imageUrl: "",
+    category: "especiais",
+  },
+  {
+    id: uuidv4(),
+    name: "Milk Shake 300ml",
+    description:
+      "Sorvete expresso + leite + cobertura + sabor a escolher + Ovomaltine (opcional)",
+    price: 12,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDHUJxa73XdPrQBSItjFKOyEvM7ichpGbmV62D",
+    category: "especiais",
+  },
+  {
+    id: uuidv4(),
+    name: "Milk Shake 400ml",
+    description:
+      "Sorvete expresso + leite + cobertura + sabor a escolher + Ovomaltine (opcional)",
+    price: 15,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDm21YkQyCdwVurFPNiSlXADcB2aJzhvpneOGt",
+    category: "especiais",
+  },
+  {
+    id: uuidv4(),
+    name: "Milk Shake Gourmet 300ml",
+    description:
+      "Sorvete expresso + leite + cobertura + sabor a escolher + Ovomaltine (opcional) + chantily",
+    price: 16,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQDiZlQ0XNgGMO3xDaYqj4Zhm75QtsdlvSc8BPI",
+    category: "especiais",
+  },
+  {
+    id: uuidv4(),
+    name: "Milk Shake Gourmet 400ml",
+    description:
+      "Sorvete expresso + leite + cobertura + sabor a escolher + Ovomaltine (opcional) + chantily",
+    price: 18,
+    imageUrl:
+      "https://lvk0y2uvlr.ufs.sh/f/loKb7FsSxkQD3ZzrWnunp9NKZ6LmTFzVMOCXPdyIluhBicQ7",
+    category: "especiais",
   },
 ];
